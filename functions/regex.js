c5bbc46e73d4482f523024feafcd5ebe17d79205
JustDams <ddw_github@de-weerd.name>
2021-05-09 18:12:37 +0200

    :sparkles: feat search with the status

---
+++ functions/regex.js
@@ -0,0 +1,51 @@
+const Big = require('big.js')
+
+const regex = /STEAM_([0-9]):([0-9]):([0-9]*)/gi
+const staticBin = '000100000000000000000001'
+
+const findSteamId = (str) => {
+  const res = []
+
+  while ((m = regex.exec(str)) !== null) {
+    if (m.index === regex.lastIndex)
+      regex.lastIndex++
+
+    const binaryId = `${decToBin(m[1], 8)}${staticBin}${decToBin(m[3], 32)}${m[2]}`
+
+    console.log(binToDec(binaryId).toString())
+    res.push(binToDec(binaryId).toString())
+  }
+
+  return res
+}
+
+const binToDec = (num) => {
+  const bin = num.split('').reverse()
+  let pow = new Big(0)
+
+  for (let i = 0; i < bin.length; i++)
+    if (bin[i] == 1)
+      pow = pow.plus(new Big(2).pow(i))
+
+  return pow
+}
+
+const decToBin = (num, len) => {
+  if (num === 0)
+    return 0
+  const bin = []
+  while (num > 0) {
+    let rem = num % 2
+    bin.push(rem)
+    num = (num / 2) === 0.5 ? 0 : Math.floor(num / 2)
+  }
+
+  while (bin.length !== len - 1)
+    bin.push(0)
+
+  return bin.reverse().join('')
+}
+
+module.exports = {
+  findSteamId,
+}
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { prefix, color, name } = require('./config.json')
 const Player = require('./functions/player')
 const Steam = require('./functions/steam')
+const RegexFun = require('./functions/regex')
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
@@ -15,40 +16,54 @@ bot.on('ready', () => {
 bot.on('message', async message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return
   else {
-    const args = message.content.slice(prefix.length).trim().split(/ +/)
-    const firstArg = args.shift().toLowerCase().split('/').filter(e => e).pop()
+    const msg = message.content.slice(prefix.length).trim()
+    const args = msg.split(/ +/)
+    const firstArg = args.shift().toLowerCase()
+    const steamParam = firstArg.split('/').filter(e => e).pop()
 
-    const steamId = await Steam.getId(firstArg)
-    try {
-      const playerId = await Player.getId(steamId)
-      const playerDatas = await Player.getDatas(playerId)
-      const playerStats = await Player.getStats(playerId)
+    const steamIds = RegexFun.findSteamId(msg)
 
-      const faceitLevel = playerDatas.games.csgo.skill_level_label
-      const faceitElo = playerDatas.games.csgo.faceit_elo
-
-      const card = new Discord.MessageEmbed()
-        .setAuthor(playerDatas.nickname, playerDatas.avatar, `https://www.faceit.com/fr/players/${playerDatas.nickname}`)
-        .setDescription(`
-          Level: **${faceitLevel}**, 
-          Elo: **${faceitElo}**, 
-          Games: **${playerStats.lifetime.Matches} (${playerStats.lifetime['Win Rate %']}% Win)**,
-          K/D: **${playerStats.lifetime['Average K/D Ratio']}**
-        `)
-        .setColor(color.levels[faceitLevel - 1])
-
-      message.channel.send(card)
-    } catch (error) {
-      console.log(error)
-      message.channel.send(
-        new Discord.MessageEmbed()
-          .setColor(color.error)
-          .setDescription('**No players found**')
-          .setFooter(`${name} Error`)
-      )
-    }
+    if (steamIds.length > 0)
+      steamIds.forEach(e => {
+        sendCardWithInfos(message, e)
+      })
+    else
+      sendCardWithInfos(message, steamParam)
   }
 })
 
 // Start the bot
 bot.login(process.env.TOKEN)
+
+
+const sendCardWithInfos = async (message, steamParam) => {
+  const steamId = await Steam.getId(steamParam)
+  try {
+    const playerId = await Player.getId(steamId)
+    const playerDatas = await Player.getDatas(playerId)
+    const playerStats = await Player.getStats(playerId)
+
+    const faceitLevel = playerDatas.games.csgo.skill_level_label
+    const faceitElo = playerDatas.games.csgo.faceit_elo
+
+    const card = new Discord.MessageEmbed()
+      .setAuthor(playerDatas.nickname, playerDatas.avatar, `https://www.faceit.com/fr/players/${playerDatas.nickname}`)
+      .setDescription(`
+        Level: **${faceitLevel}**, 
+        Elo: **${faceitElo}**, 
+        Games: **${playerStats.lifetime.Matches} (${playerStats.lifetime['Win Rate %']}% Win)**,
+        K/D: **${playerStats.lifetime['Average K/D Ratio']}**
+      `)
+      .setColor(color.levels[faceitLevel - 1])
+
+    message.channel.send(card)
+  } catch (error) {
+    console.log(error)
+    message.channel.send(
+      new Discord.MessageEmbed()
+        .setColor(color.error)
+        .setDescription('**No players found**')
+        .setFooter(`${name} Error`)
+    )
+  }
+}
