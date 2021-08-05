5436271144a079882219f6b036f7931826d7d57c
JustDams <ddw_github@de-weerd.name>
2021-08-05 15:36:37 +0200

    :recycle: refacto index to make a command folder

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const { prefix, color, name } = require('./config.json')
-const Player = require('./functions/player')
-const Steam = require('./functions/steam')
-const Graph = require('./functions/graph')
-const RegexFun = require('./functions/regex')
+const { prefix, name, color } = require('./config.json')
 const Discord = require('discord.js')
+const fs = require('fs')
 const bot = new Discord.Client()
 
 require('dotenv').config()
@@ -14,62 +11,48 @@ bot.on('ready', () => {
   bot.user.setActivity(`${prefix}`, { type: 'PLAYING' })
 })
 
+/**
+ * Setup commands
+ */
+bot.commands = new Discord.Collection()
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  command.aliasses.forEach(e => {
+    bot.commands.set(e, command)
+  })
+}
+
 bot.on('message', async message => {
   if (!message.content.startsWith(prefix) || message.author.bot) return
   else {
     const msg = message.content.slice(prefix.length).trim()
     const args = msg.split(/ +/)
-    const firstArg = args.shift().toLowerCase()
-    const steamParam = firstArg.split('/').filter(e => e).pop()
+    const command = args.shift().toLowerCase()
 
-    const steamIds = RegexFun.findSteamId(msg)
-
-    if (steamIds.length > 0)
-      steamIds.forEach(e => {
-        sendCardWithInfos(message, e)
-      })
-    else
-      sendCardWithInfos(message, steamParam)
+    if (!bot.commands.has(command))
+      message.channel.send(
+        new Discord.MessageEmbed()
+          .setColor(color.error)
+          .setDescription('**Command not found**')
+          .setFooter(`${name} Error`)
+      )
+    else {
+      try {
+        bot.commands.get(command).execute(message, args)
+      } catch (error) {
+        console.log(error)
+        message.channel.send(
+          new Discord.MessageEmbed()
+            .setColor(color.error)
+            .setDescription('**An error has occured**')
+            .setFooter(`${name} Error`)
+        )
+      }
+    }
   }
 })
 
 // Start the bot
 bot.login(process.env.TOKEN)
-
-
-const sendCardWithInfos = async (message, steamParam) => {
-  try {
-    const steamId = await Steam.getId(steamParam)
-    const playerId = await Player.getId(steamId)
-    const playerDatas = await Player.getDatas(playerId)
-    const playerStats = await Player.getStats(playerId)
-    const graphBuffer = await Graph.generateCanva(playerId)
-
-    const faceitLevel = playerDatas.games.csgo.skill_level_label
-
-    const card = new Discord.MessageEmbed()
-      .setAuthor(playerDatas.nickname, playerDatas.avatar, `https://www.faceit.com/fr/players/${playerDatas.nickname}`)
-      .setDescription(`
-        Level: **${faceitLevel}**, 
-        Elo: **${playerDatas.games.csgo.faceit_elo}**, 
-        Games: **${playerStats.lifetime.Matches} (${playerStats.lifetime['Win Rate %']}% Win)**,
-        K/D: **${playerStats.lifetime['Average K/D Ratio']}**
-      `)
-      .setColor(color.levels[faceitLevel - 1])
-
-    message.channel.send(card)
-    message.channel.send({
-      embeds: [new Discord.MessageEmbed()],
-      files: [new Discord.MessageAttachment(graphBuffer)]
-    })
-
-  } catch (error) {
-    console.log(error)
-    message.channel.send(
-      new Discord.MessageEmbed()
-        .setColor(color.error)
-        .setDescription('**No players found**')
-        .setFooter(`${name} Error`)
-    )
-  }
-}
