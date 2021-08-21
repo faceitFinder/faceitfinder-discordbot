cd5434db74b30b968299592d9855ad6e69b2c089
JustDams <ddw_github@de-weerd.name>
2021-08-21 13:52:12 +0200

    :sparkles: feat invite

---
+++ commands/invite.js
@@ -0,0 +1,19 @@
+const { name, invite } = require('../config.json')
+const Discord = require('discord.js')
+
+module.exports = {
+  name: 'invite',
+  aliasses: ['invite', 'inv'],
+  options: '',
+  description: "Get the link to invite the bot on your server.",
+  type: 'system',
+  async execute(message, args) {
+    message.channel.send(new Discord.MessageEmbed()
+      .attachFiles([
+        new Discord.MessageAttachment('./images/logo.png', 'logo.png')
+      ])
+      .setAuthor(`${name}`, 'attachment://logo.png')
+      .setDescription(`Hey <@${message.author.id}> you can invite me by clicking on the following link\n${invite}`)
+      .setFooter(`${name} Invite`))
+  }
+}
