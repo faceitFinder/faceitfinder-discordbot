e35f8a46fc6a6f78cdcd205b41baadadc69fee80
Ubuntu <ddw_github@de-weerd.name>
2021-09-15 16:00:03 +0200

    :fire: removed useless things

--- functions/client.js
+++ functions/client.js
@@ -1,17 +1,10 @@
 const { prefix } = require('../config.json')
 
-const getApp = (client, guildId) => {
-  const app = client.api.applications(client.user.id)
-  if (guildId) app.guilds(guildId)
-  return app
-}
-
 const guildCount = (client) => {
   const Guilds = client.guilds.cache.map(guild => guild.id)
   client.user.setActivity(`${prefix}help | ${Guilds.length} servers`, { type: 'PLAYING' })
 }
 
 module.exports = {
-  getApp,
   guildCount,
 }
