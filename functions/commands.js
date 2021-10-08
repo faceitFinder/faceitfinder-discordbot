4bffe2b81674ccf58519c9293364d83b9183cecf
JustDams <ddw_github@de-weerd.name>
2021-10-08 20:11:51 +0200

    :bug: fixed: stats steamids

--- functions/commands.js
+++ functions/commands.js
@@ -25,7 +25,7 @@ const getCards = async (message, array, fn, mention = 0) => {
 
 const getCardsConditions = async (mentions, steamIds, args, message, fn) => {
   mentions = new Discord.Collection(Array.from(mentions).slice(0, maxUser)),
-    steamIds = steamIds.slice(0, maxUser), steamIds = args.slice(0, maxUser)
+    steamIds = steamIds.slice(0, maxUser), message = args.slice(0, maxUser)
   if (mentions.size > 0) return getCards(message, mentions, fn, 1)
   else if (steamIds.length > 0) return getCards(message, steamIds, fn)
   else if (args.length > 0) return getCards(message, args, fn)
