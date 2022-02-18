b8efd5b7821b3213cafd16985e447c0995e559a9
JustDams <ddw_github@de-weerd.name>
2022-02-18 17:55:33 +0100

    :bug: fix issue about deathmatchs stats

--- commands/last.js
+++ commands/last.js
@@ -22,10 +22,12 @@ const sendCardWithInfos = async (message, steamParam, matchId = null) => {
   const playerId = await Player.getId(steamId)
   const playerDatas = await Player.getDatas(playerId)
   const playerHistory = (await Player.getHistory(playerId, 5))
+  const cards = []
 
   let lastMatchStats
   matchId = matchId || playerHistory.items[0].match_id
-  if (playerHistory.items.length > 0) lastMatchStats = await Match.getMatchStats(matchId)
+  if (playerHistory.items.length > 0)
+    lastMatchStats = await Match.getMatchStats(matchId).catch(err => cards.push(errorCard(err)))
   else return errorCard(`Couldn\'t get the last match of ${steamDatas.personaname}`)
 
   const faceitElo = playerDatas.games.csgo.faceit_elo
@@ -43,62 +45,65 @@ const sendCardWithInfos = async (message, steamParam, matchId = null) => {
 
   let eloDiff = lastMatchsElo.at(lastMatch?.lastMatchIndex) - lastMatchsElo.at(lastMatch?.lastMatchIndex + 1) || 0
   eloDiff = isNaN(eloDiff) ? '0' : eloDiff > 0 ? `+${eloDiff}` : eloDiff.toString()
-  const cards = []
 
   let mapThumbnail
-  lastMatchStats.rounds.forEach(r => {
-    const card = new Discord.MessageEmbed()
-    let playerStats = getPlayerStats(r.teams, playerId)
+  if (cards.length === 0)
+    lastMatchStats.rounds.forEach(r => {
+      const card = new Discord.MessageEmbed()
+      let playerStats = getPlayerStats(r.teams, playerId)
 
-    if (playerStats === undefined) cards.push(errorCard(`Couldn\'t get the stats of ${steamDatas.personaname} from his last match`).embeds[0])
-    if (lastMatchStats.rounds.length > 1) card.addFields({ name: 'round', value: `${r.match_round}/${lastMatchStats.rounds.length}` })
-    mapThumbnail = `./images/maps/${r.round_stats.Map}.jpg`
+      if (playerStats === undefined) cards.push(errorCard(`Couldn\'t get the stats of ${steamDatas.personaname} from his last match`).embeds[0])
+      if (lastMatchStats.rounds.length > 1) card.addFields({ name: 'round', value: `${r.match_round}/${lastMatchStats.rounds.length}` })
+      mapThumbnail = `./images/maps/${r.round_stats.Map}.jpg`
 
-    if (playerStats !== undefined && playerDatas !== undefined) {
-      filesAtt.push(new Discord.MessageAttachment(rankImageCanvas.toBuffer(), `${faceitLevel}.png`))
+      if (playerStats !== undefined && playerDatas !== undefined) {
+        filesAtt.push(new Discord.MessageAttachment(rankImageCanvas.toBuffer(), `${faceitLevel}.png`))
 
-      card.setAuthor({ name: playerDatas.nickname, iconURL: playerDatas.avatar, url: `https://www.faceit.com/fr/players/${playerDatas.nickname}` })
-        .setDescription(`[Steam](${steamDatas.profileurl}), [Game Lobby](https://www.faceit.com/fr/csgo/room/${matchId}/scoreboard)`)
-        .addFields({ name: 'Score', value: r.round_stats.Score.toString(), inline: true },
-          { name: 'Map', value: r.round_stats.Map, inline: true },
-          { name: 'Status', value: parseInt(playerStats.Result) ? emojis.won.balise : emojis.lost.balise, inline: true },
-          { name: 'K/D', value: playerStats['K/D Ratio'], inline: true },
-          { name: 'HS', value: `${playerStats['Headshots %']}%`, inline: true },
-          { name: 'MVPs', value: playerStats.MVPs, inline: true },
-          { name: 'Kills', value: playerStats.Kills, inline: true },
-          { name: 'Deaths', value: playerStats.Deaths, inline: true },
-          { name: 'Assists', value: playerStats.Assists, inline: true },
-          { name: 'Elo', value: eloDiff.toString(), inline: true },
-          { name: 'Date', value: new Date(lastMatch?.lastMatchStats.date).toDateString(), inline: true })
-        .setThumbnail(`attachment://${faceitLevel}.png`)
-        .setImage(`attachment://${r.round_stats.Map}.jpg`)
-        .setColor(parseInt(playerStats.Result) ? color.won : color.lost)
-        .setFooter({ text: `Steam: ${steamDatas.personaname}` })
+        card.setAuthor({ name: playerDatas.nickname, iconURL: playerDatas.avatar, url: `https://www.faceit.com/fr/players/${playerDatas.nickname}` })
+          .setDescription(`[Steam](${steamDatas.profileurl}), [Game Lobby](https://www.faceit.com/fr/csgo/room/${matchId}/scoreboard)`)
+          .addFields({ name: 'Score', value: r.round_stats.Score.toString(), inline: true },
+            { name: 'Map', value: r.round_stats.Map, inline: true },
+            { name: 'Status', value: parseInt(playerStats.Result) ? emojis.won.balise : emojis.lost.balise, inline: true },
+            { name: 'K/D', value: playerStats['K/D Ratio'], inline: true },
+            { name: 'HS', value: `${playerStats['Headshots %']}%`, inline: true },
+            { name: 'MVPs', value: playerStats.MVPs, inline: true },
+            { name: 'Kills', value: playerStats.Kills, inline: true },
+            { name: 'Deaths', value: playerStats.Deaths, inline: true },
+            { name: 'Assists', value: playerStats.Assists, inline: true },
+            { name: 'Elo', value: eloDiff.toString(), inline: true },
+            { name: 'Date', value: new Date(lastMatch?.lastMatchStats.date).toDateString(), inline: true })
+          .setThumbnail(`attachment://${faceitLevel}.png`)
+          .setImage(`attachment://${r.round_stats.Map}.jpg`)
+          .setColor(parseInt(playerStats.Result) ? color.won : color.lost)
+          .setFooter({ text: `Steam: ${steamDatas.personaname}` })
 
-      if (fs.existsSync(mapThumbnail))
-        filesAtt.push(new Discord.MessageAttachment(mapThumbnail, `${r.round_stats.Map}.jpg`))
+        if (fs.existsSync(mapThumbnail))
+          filesAtt.push(new Discord.MessageAttachment(mapThumbnail, `${r.round_stats.Map}.jpg`))
 
-      cards.push(card)
-    }
-  })
+        cards.push(card)
+      }
+    })
 
   const options = []
   for (const e of playerHistory.items) {
-    const matchStats = await Match.getMatchStats(e.match_id)
-    const maps = matchStats.rounds.map(r => r.round_stats.Map)
-    const playerResult = matchStats.rounds.map(r => getPlayerStats(r.teams, playerId)?.Result).at(0)
+    await Match.getMatchStats(e.match_id)
+      .then(ms => {
+        const maps = ms.rounds.map(r => r.round_stats.Map)
+        const playerResult = ms.rounds.map(r => getPlayerStats(r.teams, playerId)?.Result).at(0)
 
-    options.push({
-      label: new Date(e.finished_at * 1000).toDateString(),
-      description: maps.join(' '),
-      emoji: playerResult !== undefined ? parseInt(playerResult) ? emojis.won.balise : emojis.lost.balise : null,
-      default: e.match_id === matchId,
-      value: JSON.stringify({
-        u: message.author.id,
-        m: e.match_id,
-        s: steamId
+        options.push({
+          label: new Date(e.finished_at * 1000).toDateString(),
+          description: maps.join(' '),
+          emoji: playerResult !== undefined ? parseInt(playerResult) ? emojis.won.balise : emojis.lost.balise : null,
+          default: e.match_id === matchId,
+          value: JSON.stringify({
+            u: message.author.id,
+            m: e.match_id,
+            s: steamId
+          })
+        })
       })
-    })
+      .catch(err => console.log(`${err} ${e.match_id}`))
   }
 
   return {
