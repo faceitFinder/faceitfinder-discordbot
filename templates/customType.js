ff6959373e6dc7424861740f2ce46878c55fa3a2
JustDams <ddw_github@de-weerd.name>
2022-06-16 12:34:58 +0200

    :fire: removed pagination stuff

--- templates/customType.js
+++ templates/customType.js
@@ -3,9 +3,5 @@ const { color } = require('../config.json')
 module.exports.TYPES = {
   ELO: { name: 'ELO', emoji: '📈', color: color.levels, style: 'SECONDARY' },
   KD: { name: 'K/D', emoji: '📉', color: color.kd, style: 'SECONDARY' },
-  NEXT: { name: 'Next', emoji: '▶', style: 'PRIMARY' },
-  PREV: { name: 'Prev', emoji: '◀', style: 'PRIMARY' },
-  START: { name: 'Start', emoji: '⏮', style: 'PRIMARY' },
-  END: { name: 'End', emoji: '⏭', style: 'PRIMARY' },
 }
 module.exports.getType = (t) => Object.entries(this.TYPES).filter(e => e[1].name === t)[0][1]
