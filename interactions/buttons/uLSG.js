1d477691e60dcc71044b26f074020fb5003db677
JustDams <ddw_github@de-weerd.name>
2022-06-16 23:09:46 +0200

    :sparkles: feat: laststats

---
+++ interactions/buttons/uLSG.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const { getDefaultInteractionOption } = require('../../functions/commands')
+const { getCardWithInfos } = require('../../functions/dateStats')
+const CustomType = require('../../templates/customType')
+const loadingCard = require('../../templates/loadingCard')
+
+module.exports = {
+  name: 'uLSG',
+  async execute(interaction, json) {
+    const values = getDefaultInteractionOption(interaction).value
+    json = { ...json, ...JSON.parse(values) }
+
+    if (interaction.user.id !== json.u) return false
+
+    loadingCard(interaction)
+
+    const actionRow = interaction.message.components.at(0)
+
+    return await getCardWithInfos(actionRow, json, CustomType.getType(interaction.component.label), json.m, 'uLSG')
+  }
+}
