9b64cf5700176a4873069bb586ae1ffc501d05c5
JustDams <ddw_github@de-weerd.name>
2022-06-20 19:04:33 +0200

    :recycle: refactor: pagination

--- functions/pagination.js
+++ functions/pagination.js
@@ -12,19 +12,19 @@ const getPagination = (page, maxPage, id) => {
   return new Discord.MessageActionRow()
     .addComponents([
       CustomTypeFunc.generateButtons(
-        { id: id, page: maxPage, c: page, n: 1 },
+        { id: id, page: 0, c: page, n: 1 },
         CustomType.TYPES.FIRST,
         page === 0),
       CustomTypeFunc.generateButtons(
-        { id: id, page: page + 1, c: page, n: 3 },
+        { id: id, page: page - 1, c: page, n: 3 },
         CustomType.TYPES.PREV,
         !(page - 1 >= 0)),
       CustomTypeFunc.generateButtons(
-        { id: id, page: page - 1, c: page, n: 2 },
+        { id: id, page: page + 1, c: page, n: 2 },
         CustomType.TYPES.NEXT,
         !(page + 1 <= maxPage)),
       CustomTypeFunc.generateButtons(
-        { id: id, page: 0, c: page, n: 4 },
+        { id: id, page: maxPage, c: page, n: 4 },
         CustomType.TYPES.LAST,
         page === maxPage),
     ])
