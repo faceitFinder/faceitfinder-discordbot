1296bec2cc18279f3de176f702790d512de214ad
JustDams <ddw_github@de-weerd.name>
2022-07-23 23:15:53 +0200

    :bug: fix merge

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const { Client, GatewayIntentBits } = require('discord.js')
 const fs = require('fs')
 const AntiSpam = require('./templates/antispam')
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] })
-<<<<<<< HEAD
 const { updateRoles } = require('./functions/roles')
-=======
->>>>>>> 32fe027373a465b1df5f9e1024227ea8448436c0
 
 require('dotenv').config()
 
@@ -18,8 +15,5 @@ fs.readdirSync('./events').filter(file => file.endsWith('.js')).forEach(async (f
 
 // Start the bot
 client.login(process.env.TOKEN)
-<<<<<<< HEAD
 
 setInterval(() => { updateRoles(client) }, 3600000)
-=======
->>>>>>> 32fe027373a465b1df5f9e1024227ea8448436c0
