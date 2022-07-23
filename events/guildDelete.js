820598fc81e7445d7148a3c8c8030b20f7df5450
Damien <48047820+JustDams@users.noreply.github.com>
2022-07-23 23:37:38 +0200

    Revert "Feat/roles"

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,10 +1,8 @@
-const GuildRoles = require('../database/guildRoles')
 const { guildCount } = require('../functions/client')
 
 module.exports = {
   name: 'guildDelete',
   async execute(guild) {
     guildCount(guild.client)
-    await GuildRoles.remove(guild.id)
   }
 }
