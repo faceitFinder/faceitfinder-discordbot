6305ce714827e339e6c3a0aa80dab4108c143e49
JustDams <ddw_github@de-weerd.name>
2022-07-25 16:25:05 +0200

    :sparkles: feat: remove role on unlink && add role on link

--- commands/unlink.js
+++ commands/unlink.js
@@ -1,15 +1,14 @@
 const User = require('../database/user')
+const { updateRoles } = require('../functions/roles')
 const errorCard = require('../templates/errorCard')
 const successCard = require('../templates/successCard')
 
 const sendCardWithInfos = async (interaction) => {
   const discordId = interaction.user.id
   if (await User.exists(discordId)) {
-    User.remove(discordId)
-
+    updateRoles(interaction.client, discordId, null, true)
     return successCard('Your account has been unlinked.')
-  }
-  else return errorCard('Your account is not linked to a user.')
+  } else return errorCard('Your account is not linked to a user.')
 }
 
 module.exports = {
