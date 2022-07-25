1fc9e42eb079b381b36a0f5097cceb2e57962656
JustDams <ddw_github@de-weerd.name>
2022-07-25 21:57:45 +0200

    :sparkles: feat: setup custom roles & more

--- commands/roles.js
+++ commands/roles.js
@@ -1,16 +1,66 @@
 const Discord = require('discord.js')
 const { color, name } = require('../config.json')
-const { isInteractionSubcommandEqual } = require('../functions/commands')
+const { isInteractionSubcommandEqual, getInteractionOption } = require('../functions/commands')
 const errorCard = require('../templates/errorCard')
 const GuildRoles = require('../database/guildRoles')
 const successCard = require('../templates/successCard')
 const { updateRoles } = require('../functions/roles')
 
 const SETUP = 'setup'
+const GENERATE = 'generate'
 const REMOVE = 'remove'
 
 const setupRoles = async (interaction) => {
-  await removeRoles(interaction)
+  if (getInteractionOption(interaction, 'remove_old')) await removeRoles(interaction)
+
+  const roles = []
+  const rolesFields = []
+  const botRole = interaction.guild.roles.botRoleFor(interaction.client.user)
+  let error = 0
+
+  for (let i = 1; i <= 10; i++) {
+    const roleId = getInteractionOption(interaction, `level_${i}`)
+    const role = await interaction.guild.roles.fetch(roleId)
+
+    const comparaison = interaction.guild.roles.comparePositions(botRole, role)
+    if (comparaison < 0) {
+      error++
+      rolesFields.push('This role is higher than the bot role, please place it below the bot role.')
+    } else {
+      rolesFields.push(`<@&${roleId}>`)
+      roles.push(roleId)
+    }
+  }
+
+  const card = new Discord.EmbedBuilder()
+    .setAuthor({ name: name, iconURL: 'attachment://logo.png' })
+    .setFooter({ text: `${name} Infos` })
+
+
+  rolesFields.reverse().forEach((v, i) => card.addFields({ name: `Level ${10 - i}`, value: v, inline: true }))
+
+  if (error === 0) {
+    await GuildRoles.remove(interaction.guild.id)
+    GuildRoles.create(interaction.guild.id, roles[0], roles[1], roles[2], roles[3], roles[4], roles[5], roles[6], roles[7], roles[8], roles[9])
+
+    await updateRoles(interaction.client, null, interaction.guild.id)
+
+    card.setColor(color.primary)
+      .setDescription('The roles have been setup successfully')
+  } else {
+    card.setColor(color.error)
+      .setDescription('One or more roles are invalid')
+      .setFooter({ text: `${name} Error` })
+  }
+
+  return {
+    embeds: [card],
+    files: [new Discord.AttachmentBuilder('./images/logo.png', { name: 'logo.png' })]
+  }
+}
+
+const generateRoles = async (interaction) => {
+  if (getInteractionOption(interaction, 'remove_old')) await removeRoles(interaction)
 
   const roles = []
 
@@ -24,8 +74,8 @@ const setupRoles = async (interaction) => {
       .catch(console.error)
   }
 
-  GuildRoles.create(interaction.guild.id,
-    roles[9], roles[8], roles[7], roles[6], roles[5], roles[4], roles[3], roles[2], roles[1], roles[0])
+  await GuildRoles.remove(interaction.guild.id)
+  GuildRoles.create(interaction.guild.id, roles[9], roles[8], roles[7], roles[6], roles[5], roles[4], roles[3], roles[2], roles[1], roles[0])
 
   await updateRoles(interaction.client, null, interaction.guild.id)
 
@@ -60,9 +110,89 @@ module.exports = {
   options: [
     {
       name: SETUP,
+      description: 'Setup the roles that you want for each ranks on the server.',
+      type: Discord.ApplicationCommandOptionType.Subcommand,
+      slash: true,
+      options: [
+        {
+          name: 'level_1',
+          description: 'The role for the level 1',
+          type: Discord.ApplicationCommandOptionType.Role,
+          required: true
+        },
+        {
+          name: 'level_2',
+          description: 'The role for the level 2',
+          type: Discord.ApplicationCommandOptionType.Role,
+          required: true
+        },
+        {
+          name: 'level_3',
+          description: 'The role for the level 3',
+          type: Discord.ApplicationCommandOptionType.Role,
+          required: true
+        },
+        {
+          name: 'level_4',
+          description: 'The role for the level 4',
+          type: Discord.ApplicationCommandOptionType.Role,
+          required: true
+        },
+        {
+          name: 'level_5',
+          description: 'The role for the level 5',
+          type: Discord.ApplicationCommandOptionType.Role,
+          required: true
+        },
+        {
+          name: 'level_6',
+          description: 'The role for the level 6',
+          type: Discord.ApplicationCommandOptionType.Role,
+          required: true
+        },
+        {
+          name: 'level_7',
+          description: 'The role for the level 7',
+          type: Discord.ApplicationCommandOptionType.Role,
+          required: true
+        },
+        {
+          name: 'level_8',
+          description: 'The role for the level 8',
+          type: Discord.ApplicationCommandOptionType.Role,
+          required: true
+        },
+        {
+          name: 'level_9',
+          description: 'The role for the level 9',
+          type: Discord.ApplicationCommandOptionType.Role,
+          required: true
+        },
+        {
+          name: 'level_10',
+          description: 'The role for the level 10',
+          type: Discord.ApplicationCommandOptionType.Role,
+          required: true
+        },
+        {
+          name: 'remove_old',
+          description: 'Remove the old roles if they exist.',
+          type: Discord.ApplicationCommandOptionType.Boolean,
+        }
+      ]
+    },
+    {
+      name: GENERATE,
       description: 'Generates the rank roles on the server.',
       type: Discord.ApplicationCommandOptionType.Subcommand,
-      slash: true
+      slash: true,
+      options: [
+        {
+          name: 'remove_old',
+          description: 'Remove the old roles if they exist.',
+          type: Discord.ApplicationCommandOptionType.Boolean,
+        }
+      ]
     },
     {
       name: REMOVE,
@@ -82,6 +212,7 @@ module.exports = {
       return errorCard('I don\'t have the permission to manage roles')
 
     if (isInteractionSubcommandEqual(interaction, SETUP)) return await setupRoles(interaction)
+    if (isInteractionSubcommandEqual(interaction, GENERATE)) return await generateRoles(interaction)
     if (isInteractionSubcommandEqual(interaction, REMOVE)) return await removeRoles(interaction)
       .then(() => successCard('The roles have been removed'))
   }
