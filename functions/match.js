593683c6be4445b597cf2b25ea4737b782c4239f
Damien <48047820+JustDams@users.noreply.github.com>
2023-02-12 23:06:03 +0100

    fix: error messages on 404 (#260)

--- functions/match.js
+++ functions/match.js
@@ -2,14 +2,11 @@ const axios = require('axios')
 const Faceit = require('./faceit')
 
 const getMatchElo = (playerId, limit = 20, page = 0) => axios.get(`https://api.faceit.com/stats/api/v1/stats/time/users/${playerId}/games/csgo?size=${limit}&page=${page}`)
-  .then(res => {
-    if (res.status == 200) return res.data
-    else {
-      console.error(res.statusText, res.url)
-      throw 'Couldn\'t get matches'
-    }
+  .then(res => res.data)
+  .catch(e => {
+    console.error(e.response.status, e.response.statusText, playerId)
+    throw 'Couldn\'t get matches'
   })
-  .then(data => data)
 
 const getMatchStats = matchid => Faceit.fetchData(`https://open.faceit.com/data/v4/matches/${matchid}/stats`, 'Couldn\'t get matches stats')
 
