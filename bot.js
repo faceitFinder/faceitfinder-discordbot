1d77c7af5143f51384a88ca87dc9c5832cd96a50
Damien <48047820+JustDams@users.noreply.github.com>
2023-04-22 01:38:47 +0200

    fix: auto update && optimised it (#327)

---
+++ bot.js
@@ -0,0 +1,21 @@
+const { Client, GatewayIntentBits } = require('discord.js')
+const fs = require('fs')
+const AntiSpam = require('./templates/antispam')
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages] })
+const { updateRoles } = require('./functions/roles')
+const mongo = require('./database/mongo')
+
+require('dotenv').config()
+mongo().then(() => { console.info('🧱 Connected to mongo') }).catch(console.error)
+
+client.antispam = new AntiSpam()
+
+fs.readdirSync('./events').filter(file => file.endsWith('.js')).forEach(async (file) => {
+  const event = require(`./events/${file}`)
+  client.on(event.name, (...args) => { event.execute(...args) })
+})
+
+// Start the bot
+client.login(process.env.TOKEN)
+
+setInterval(() => { updateRoles(client) }, 3600000)
