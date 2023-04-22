49146c1a3858991c42d97476c2f82f20b7f5245d
JustDams <ddw_github@de-weerd.name>
2023-04-22 08:05:03 -0700

    update: laststats buttons

--- interactions/buttons/uLSG.js
+++ interactions/buttons/uLSG.js
@@ -1,8 +1,7 @@
 const CommandsStats = require('../../database/commandsStats')
-const { updateMessage } = require('../../events/interactionCreate')
 const { getDefaultInteractionOption } = require('../../functions/commands')
 const { getTypeGraph } = require('../../functions/commandStats')
-const { getCardWithInfo } = require('../../functions/dateStats')
+const { getCardWithInfo, updateOptions } = require('../../functions/dateStats')
 const CustomType = require('../../templates/customType')
 const loadingCard = require('../../templates/loadingCard')
 
@@ -18,6 +17,10 @@ module.exports = {
 
     const actionRow = interaction.message.components.at(0)
     const [from, to] = interaction.message.embeds.at(0).data.fields[0].value.split('\n').map(e => new Date(e.trim()))
+
+    const options = updateOptions(actionRow.components, JSON.stringify(json))
+    options.at(0).default = true
+    actionRow.components.at(0).options = options
 
     json.f = from.getTime() / 1000
     json.t = to.setHours(+24) / 1000
