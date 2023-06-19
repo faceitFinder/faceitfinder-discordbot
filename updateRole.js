2109beb46b62716b398efb311181449f32493d3c
Damien <48047820+JustDams@users.noreply.github.com>
2023-06-19 21:01:11 +0200

    Fix/role attribution (#361)
    * update: script

--- updateRole.js
+++ updateRole.js
@@ -6,15 +6,19 @@ const mongo = require('./database/mongo')
 
 mongo().then(() => { console.info('🧱 Connected to mongo') }).catch(console.error)
 
-GuildRoles.getAll()
-  .then((guildRoles) =>
-    guildRoles.forEach((roles) => {
-      getRoleIds(roles)
-        .forEach(async (roleId, i) =>
-          GuildCustomRole.create(roles.id, roleId, color.levels[i + 1].min, color.levels[i + 1].max)
-        )
-    }))
-  .then(() => console.info('✅ Roles updated'))
-  .catch(console.error)
-  .finally(() => process.exit(0))
 
+GuildRoles.getAll().then(async (guildRoles) => {
+  for (let i = 0; i < guildRoles.length; i++) {
+    const guildRole = guildRoles[i]
+
+    getRoleIds(guildRole).forEach(async (roleId, i) => {
+      if (await GuildCustomRole.getOne(guildRole.id, roleId)) {
+        console.info(`⚠️ Role ${roleId} already exists for guild ${guildRole.id}`)
+        return
+      }
+
+      GuildCustomRole.create(guildRole.id, roleId, color.levels[i + 1].min, color.levels[i + 1].max)
+      console.info(`✅ Created role ${roleId} for guild ${guildRole.id}`)
+    })
+  }
+})
