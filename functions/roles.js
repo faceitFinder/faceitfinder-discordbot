af2d81a0c48dda59fbccfaf0f0c33036a6bf6a93
JustDams <ddw_github@de-weerd.name>
2023-06-19 11:25:20 -0700

    update: roles

--- functions/roles.js
+++ functions/roles.js
@@ -1,4 +1,3 @@
-const GuildRoles = require('../database/guildRoles')
 const GuildCustomRole = require('../database/guildCustomRole')
 const User = require('../database/user')
 const { getFaceitPlayerDatas } = require('./player')
@@ -6,13 +5,14 @@ const { getFaceitPlayerDatas } = require('./player')
 const getRoleIds = (guildRoles) => Object.keys(Object.entries(guildRoles)[2][1])
   .filter(e => e.startsWith('level')).map(e => guildRoles[e])
 
-const setupRoles = async (client, user, guildRoles, remove) => {
-  const guildId = guildRoles.guildId ?? guildRoles.id
+const setupRoles = async (client, user, guildId, remove) => {
   const guildDatas = await client.guilds.fetch(guildId)
   let members
 
   if (user) members = [await guildDatas.members.fetch(user.at(0).discordId).catch(() => null)]
   else members = await guildDatas.members.fetch({ cache: false })
+
+  const roles = await GuildCustomRole.getRolesOf(guildDatas.id)
 
   members?.forEach(async (member) => {
     if (!member) return
@@ -24,55 +24,41 @@ const setupRoles = async (client, user, guildRoles, remove) => {
     user = user.flat().at(0)
 
     const playerDatas = await getFaceitPlayerDatas(user.faceitId).catch(console.error)
-
     if (!playerDatas?.games?.csgo) return
 
-    const playerLevel = playerDatas.games.csgo.skill_level
     const playerElo = playerDatas.games.csgo.faceit_elo
-    const rolesToAdd = []
-
-    if (guildRoles?.guildId) {
-      rolesToAdd.push({
-        roleId: guildRoles.roleId,
-        remove: !(playerElo >= guildRoles.eloMin && playerElo <= guildRoles.eloMax && !remove)
-      })
-    } else {
-      const roleLevels = getRoleIds(guildRoles)
-      rolesToAdd.push({
-        roleId: roleLevels[playerLevel - 1],
-        remove
-      })
-    }
 
     if (user.nickname) await member.edit({ nick: playerDatas.nickname }).catch(console.error)
 
-    rolesToAdd.forEach(async (role) => {
-      const {
-        roleId,
-        remove
-      } = role
-
+    roles.forEach(async (role) => {
+      const removeRole = remove || playerElo < role.eloMin || playerElo > role.eloMax
+      const roleId = role.roleId
       const rolesFit = member.roles.resolve(roleId)
 
-      if (remove || !rolesFit) await member.roles.remove(roleId).catch(console.error)
-      if (!remove && !rolesFit) await member.roles.add(roleId).catch(console.error)
+      if (removeRole || !rolesFit) await member.roles.remove(roleId).catch((err) => handleRoleErrors(err, role))
+      if (!removeRole && !rolesFit) await member.roles.add(roleId).catch((err) => handleRoleErrors(err, role))
     })
   })
 }
 
 const updateRoles = async (client, discordId, guildId, remove = false) => {
-  let user, guildRoles
+  let user, guilds
 
   if (discordId) user = await User.get(discordId)
 
-  if (guildId) guildRoles = [await GuildRoles.getRolesOf(guildId), await GuildCustomRole.getRolesOf(guildId)].flat()
-  else guildRoles = [await GuildRoles.getAll(), await GuildCustomRole.getAll()].flat()
+  if (guildId) guilds = [guildId].flat()
+  else guilds = (await GuildCustomRole.getAll()).map(e => e.guildId).filter((e, i, a) => a.indexOf(e) === i)
 
-  Promise.all(guildRoles.map(async guildRoles => await setupRoles(client, user, guildRoles, remove).catch(console.error)))
+  Promise.all(guilds.map(async guild => await setupRoles(client, user, guild, remove).catch(console.error)))
     .then(() => { if (remove) User.remove(discordId, guildId) })
     .catch(console.error)
 }
 
+const handleRoleErrors = (err, role) => {
+  if (err.status === '404') GuildCustomRole.remove(role.guildId, role.roleId)
+}
+
 module.exports = {
-  updateRoles
+  updateRoles,
+  getRoleIds
 }
