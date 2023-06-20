cf626eb06e9133b8b82f2b8dcb94d564b4b9b1f0
JustDams <ddw_github@de-weerd.name>
2023-06-20 10:41:08 -0700

    delete: catch on role updates

--- functions/roles.js
+++ functions/roles.js
@@ -9,13 +9,13 @@ const setupRoles = async (client, user, guildId, remove) => {
   const guildDatas = await client.guilds.fetch(guildId)
   let members
 
-  if (user) members = [await guildDatas.members.fetch(user.at(0).discordId).catch(() => null)]
+  if (user.length > 0) members = [await guildDatas.members.fetch({ user: user.at(0).discordId, cache: false }).catch(() => null)]
   else members = await guildDatas.members.fetch({ cache: false })
 
   const roles = await GuildCustomRole.getRolesOf(guildDatas.id)
 
   members?.forEach(async (member) => {
-    if (!member) return
+    if (!member?.user) return
     let user = await User.get(member.user.id)
 
     if (!(user.length > 0)) return
@@ -23,12 +23,12 @@ const setupRoles = async (client, user, guildId, remove) => {
 
     user = user.flat().at(0)
 
-    const playerDatas = await getFaceitPlayerDatas(user.faceitId).catch(console.error)
+    const playerDatas = await getFaceitPlayerDatas(user.faceitId).catch(() => null)
     if (!playerDatas?.games?.csgo) return
 
     const playerElo = playerDatas.games.csgo.faceit_elo
 
-    if (user.nickname) await member.edit({ nick: playerDatas.nickname }).catch(console.error)
+    if (user.nickname) await member.edit({ nick: playerDatas.nickname }).catch(() => null)
 
     roles.forEach(async (role) => {
       const removeRole = remove || playerElo < role.eloMin || playerElo > role.eloMax
@@ -44,7 +44,7 @@ const setupRoles = async (client, user, guildId, remove) => {
 const updateRoles = async (client, discordId, guildId, remove = false) => {
   let user, guilds
 
-  if (discordId) user = await User.get(discordId)
+  if (discordId) user = [await User.get(discordId)].flat()
 
   if (guildId) guilds = [guildId].flat()
   else guilds = (await GuildCustomRole.getAll()).map(e => e.guildId).filter((e, i, a) => a.indexOf(e) === i)
