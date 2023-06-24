b2ea9262ec06cbb1a31d512f09b993556e980125
JustDams <ddw_github@de-weerd.name>
2023-06-24 13:22:19 -0700

    fix: find, update error trace

--- functions/error.js
+++ functions/error.js
@@ -1,4 +1,5 @@
 const { logChannel } = require('../config.json')
+const { getInteractionOption } = require('./commands')
 
 module.exports = (interaction, error) => {
   interaction.followUp().catch(() => null)
@@ -11,9 +12,20 @@ Guild: ${interaction.guild.name} (${interaction.guild.id})
 Channel: #${interaction.channel.name} (${interaction.channel.id})
 User: ${interaction.user.tag} (${interaction.user.id})
 Date: ${new Date().toLocaleString('fr-FR', { timeZone: 'Europe/Paris', timeStyle: 'short', dateStyle: 'long' })}
+Command: ${interaction.commandName}
+Options: 
+${getOptionsKeyValues(interaction)}
 ${Error(error).stack}
 \`\`\`
       `,
     })
   }).catch(console.error)
+}
+
+const getOptionsKeyValues = (interaction) => {
+  const keys = interaction.client.commands.get(interaction.commandName).options.map(option => option.name)
+  if (!keys.length) return ''
+
+  const keyValues = keys.map(key => `- ${key}: ${getInteractionOption(interaction, key) ?? ''}`)
+  return keyValues.join('\n')
 }
