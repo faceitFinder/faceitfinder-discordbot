9c1cabfd4e7bc95f6fba5f6468c0dfcd3f22ef20
JustDams <ddw_github@de-weerd.name>
2023-10-11 12:57:26 -0700

    feat: date stats with game

--- commands/yearstats.js
+++ commands/yearstats.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const errorCard = require('../templates/errorCard')
 const DateStats = require('../functions/dateStats')
-const { getCardsConditions } = require('../functions/commands')
+const { getCardsConditions, getGameOption } = require('../functions/commands')
 const CustomType = require('../templates/customType')
 const Options = require('../templates/options')
 const { getPageSlice, getMaxPage } = require('../functions/pagination')
@@ -16,13 +16,16 @@ const getYear = date => {
   return date.getTime()
 }
 
-const sendCardWithInfo = async (interaction, playerParam, page = 0) => {
+const sendCardWithInfo = async (interaction, playerParam, page = 0, game) => {
+  game ??= getGameOption(interaction)
+
   const {
     playerDatas,
     playerHistory
   } = await getStats({
     playerParam,
-    matchNumber: 0
+    matchNumber: 0,
+    game
   })
 
   const playerId = playerDatas.player_id
@@ -42,8 +45,7 @@ const sendCardWithInfo = async (interaction, playerParam, page = 0) => {
       .setValue(JSON.stringify({
         s: playerId,
         f: from.getTime() / 1000,
-        t: to / 1000,
-        u: interaction.user.id
+        t: to / 1000
       }))
 
     options.push(option)
@@ -67,13 +69,16 @@ const sendCardWithInfo = async (interaction, playerParam, page = 0) => {
 
   return DateStats.getCardWithInfo(
     interaction,
-    row,
+    DateStats.buildRows(row, interaction, game, 'strings.selectYear'),
     JSON.parse(pagination[0].data.value),
     CustomType.TYPES.ELO,
     'uDSG',
     0,
     getMaxPage(options),
-    page
+    page,
+    null,
+    false,
+    game
   )
 }
 
