e25e77a9381e56af04a4d15e4d53e8b663584706
JustDams <ddw_github@de-weerd.name>
2023-10-26 14:31:52 -0700

    update: datestats pagination handling

--- interactions/buttons/uDSG.js
+++ interactions/buttons/uDSG.js
@@ -1,37 +1,41 @@
+const { Interaction } = require('discord.js')
 const CommandsStats = require('../../database/commandsStats')
-const { sendCardWithInfo } = require('../selectmenus/dateStatsSelector')
-const CustomType = require('../../templates/customType')
-const { loadingCard } = require('../../templates/loadingCard')
-const { getDefaultInteractionOption, getOptionsValues } = require('../../functions/commands')
+const DateStats = require('../../functions/dateStats')
+const { getCardByUserType } = require('../../templates/loadingCard')
+const { updateButtons } = require('../../functions/customType')
+const { disabledOptions } = require('../../functions/pagination')
+const { getTypeFromEmoji } = require('../../templates/customType')
 
 /**
  * Update date stats graph.
  */
 module.exports = {
   name: 'uDSG',
-  async execute(interaction, json) {
+  async execute(interaction, json, newUser = false) {
     const commandName = await interaction.message.fetchReference()
       .then((message) => message.interaction.commandName)
       .catch(() => interaction.message.interaction.commandName)
     CommandsStats.create(commandName, `button - ${json.t.name}`, interaction)
 
-    loadingCard(interaction)
+    let components = interaction.message.components
 
-    return sendCardWithInfo(interaction, json, CustomType.getType(interaction.component.label))
-  },
-  getJSON(interaction, json) {
-    const values = JSON.parse(getDefaultInteractionOption(interaction, 1).value)
-    const interactionValues = getOptionsValues(interaction)
-    const dataRow = interaction.message.components.at(0)
+    getCardByUserType(newUser, interaction)
 
-    return Object.assign({}, json, values, interactionValues, { dataRow })
-  },
-  updateUser(interaction) {
-    const values = this.getJSON(interaction)
-    const dataRowValues = JSON.parse(values.dataRow.components.at(0).options.at(0).value)
-    dataRowValues.u = interaction.user.id
-    values.dataRow.components.at(0).options.at(0).value = JSON.stringify(dataRowValues)
+    const resp = await DateStats.getCardWithInfo({
+      interaction,
+      values: json,
+      type: json.t
+    })
 
-    return values
+    // Select menu
+    components.at(0).components.at(0).setDisabled(false)
+    // Graph buttons
+    components.at(1).components = updateButtons(components.at(1).components, json.t)
+    // Pagination buttons
+    components.at(2).components.forEach((button) => button.setDisabled(!!disabledOptions(json.cp, json.mp, getTypeFromEmoji(button.data.emoji.name))))
+
+    resp.components = components
+
+    return resp
   }
 }
