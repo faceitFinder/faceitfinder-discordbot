14c4e449e372b7f57b1a00c734222981322560b4
Damien <48047820+JustDams@users.noreply.github.com>
2025-01-24 17:40:37 +0100

    feat/adr (#450)

--- functions/dateStats.js
+++ functions/dateStats.js
@@ -246,9 +246,8 @@ const generateDateStatsFields = (playerLastStats, head) => {
     { name: 'Avg Kills', value: playerLastStats['Average Kills'].toFixed(2), inline: true },
     { name: 'Avg Deaths', value: playerLastStats['Average Deaths'].toFixed(2), inline: true },
     { name: 'Avg Assists', value: playerLastStats['Average Assists'].toFixed(2), inline: true },
-    // Removed from faceit endpoint used by the api
     // { name: 'Avg Damages', value: playerLastStats['Average Damages']?.toString() ?? 'N/A', inline: true },
-    // { name: 'Avg ADR', value: playerLastStats['Average ADR']?.toString() ?? 'N/A', inline: true },
+    { name: 'Avg ADR', value: playerLastStats['Average ADR']?.toString() ?? 'N/A', inline: false },
     // { name: 'Avg Entries (WR)', value: entries, inline: true },
     { name: 'Red K/D', value: playerLastStats['Red K/D'].toString(), inline: true },
     { name: 'Orange K/D', value: playerLastStats['Orange K/D'].toString(), inline: true },
