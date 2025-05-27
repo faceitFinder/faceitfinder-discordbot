5ec4c390dd4e8c969e2ebc744b15f9423fc98eab
JustDams <noreply@github.com>
2025-05-27 10:48:49 +0000

    update: guildcount

--- functions/client.js
+++ functions/client.js
@@ -1,15 +1,16 @@
 const { Api } = require('@top-gg/sdk')
 const { ActivityType } = require('discord.js')
 
-const guildCount = (client) => {
-  const Guilds = client.guilds.cache.map(guild => guild.id)
-  client.user.setActivity(`/help | ${Guilds.length} servers`, { type: ActivityType.Playing })
+const guildCount = async (client) => {
+  const guildsSize = await client.shard.fetchClientValues('guilds.cache.size')
+    .then(results => results.reduce((acc, guildCount) => acc + guildCount, 0))
+  client.user.setActivity(`/help | ${guildsSize} Guilds`, { type: ActivityType.Custom })
 
   // Send datas to top.gg
   if (process.env.TOPGG_TOKEN) {
     const api = new Api(process.env.TOPGG_TOKEN)
     api.postStats({
-      serverCount: Guilds.length
+      serverCount: guildsSize,
     }).catch(console.error)
   }
 }
