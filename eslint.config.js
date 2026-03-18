d088975fcac60143fc2eecf1d57ecc0d1f341b0f
JustDams <noreply@github.com>
2026-03-18 21:11:08 +0000

    update: esling config

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,25 @@
-module.exports = {
-  'env': {
-    'node': true,
-    'commonjs': true,
-    'es2021': true
-  },
-  'parserOptions': {
-    'ecmaVersion': 13
-  },
-  'ignorePatterns': [
-    'images/*',
-    'node_modules/*',
-  ],
-  'rules': {
-    'indent': [
-      'error',
-      2
+// eslint.config.js (Flat Config CommonJS)
+const js = require('@eslint/js')
+const globals = require('globals')
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: globals.node
+    },
+    ignores: [
+      'images/**',
+      'node_modules/**'
     ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
-    'camelcase': [
-      'error',
-      {
-        properties: 'always'
-      }
-    ]
+    rules: {
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      camelcase: ['error', { properties: 'always' }]
+    }
   }
-}
+]
